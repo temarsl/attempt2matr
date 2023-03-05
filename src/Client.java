7c3ee58dce71255e4d46a39a9dfcdd08e00a6419
temarsl <arslanova.maryam@gmail.com>
2023-03-06 00:00:36 +0300

    math_chat1

---
+++ src/Client.java
@@ -0,0 +1,39 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
+
+    public static void main(String[] args) {
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+        ) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Who are you?");
+            String msg = scanner.nextLine(); // Вася
+            output.writeUTF(msg); // отправляем сообщение на сервер
+            String receivedMsg = input.readUTF(); // получаем ответ от сервера
+            System.out.println("Received from the server: " + receivedMsg); // получено с сервера
+
+            receivedMsg = input.readUTF();
+            System.out.println("Received from the server: " + receivedMsg); // получено с сервера
+            output.writeUTF(scanner.nextLine());
+            receivedMsg = input.readUTF();
+            System.out.println("Received from the server: " + receivedMsg); // получено с сервера
+            output.writeUTF(scanner.nextLine());
+
+            receivedMsg = input.readUTF();
+            System.out.println("Received from the server: " + receivedMsg); // получено с сервера
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("enter your name");
-        String name = scan.nextLine();
-        System.out.println("Hello, " + name);
-        System.out.println("enter x");
-        double x = scan.nextDouble();
-        System.out.println("enter y");
-        double y = scan.nextDouble();
-        System.out.println(3*Math.cos(x - Math.PI)*Math.cos(x - Math.PI)/(0.5 + Math.sin(y*y)));
+    private static final int PORT = 34522;
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                try (
+                        Socket socket = server.accept(); // принимаем нового клиента
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+                    String msg = input.readUTF(); // прочитать сообщение от клиента
+                    output.writeUTF("Hello, " + msg); // отправить сообщение клиенту
+                    System.out.println(msg + " came to us");
+
+                    output.writeUTF("enter x" ); // отправить сообщение клиенту
+                    msg = input.readUTF();
+                    double x = Double.parseDouble(msg);
+
+                    output.writeUTF("enter y" ); // отправить сообщение клиенту
+                    msg = input.readUTF();
+                    double y = Double.parseDouble(msg);
+
+                    double ans = (3*Math.cos(x - Math.PI)*Math.cos(x - Math.PI)/(0.5 + Math.sin(y*y)));
+                    output.writeUTF("ans = " + ans);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
