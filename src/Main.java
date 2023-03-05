8b6a348982ef9ac589d2cd035fd6032220e59b2f
temarsl <arslanova.maryam@gmail.com>
2023-03-05 17:36:35 +0300

    master 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
