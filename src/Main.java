58bb757ef435a897b60e75e9c8ae654c04ded93d
temarsl <arslanova.maryam@gmail.com>
2023-03-05 17:38:18 +0300

    master 2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
         System.out.println("enter your name");
         String name = scan.nextLine();
         System.out.println("Hello, " + name);
+        System.out.println("enter x");
+        double x = scan.nextDouble();
+        System.out.println("enter y");
+        double y = scan.nextDouble();
+        System.out.println(3*Math.cos(x - Math.PI)*Math.cos(x - Math.PI)/(0.5 + Math.sin(y*y)));
     }
 }
